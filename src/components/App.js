1873084fe0486e52e4f5cad2fd5114f40e229846
Vikasagrwl <105685053+Vikasagrwl@users.noreply.github.com>
2023-04-16 11:42:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import "../styles/App.css"
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
 
-const WORD_LIST = ['apple', 'banana', 'cherry', 'grape', 'orange'];
+const WORD_LIST = ["apple", "banana", "cherry", "grape", "orange"];
 
 function App() {
-  const [word, setWord] = useState('');
+  const [word, setWord] = useState("");
   const [flashWord, setFlashWord] = useState(true);
-  const [userInput, setUserInput] = useState('');
-  const [result, setResult] = useState('');
+  const [userInput, setUserInput] = useState("");
+  const [result, setResult] = useState("");
   const [index, setIndex] = useState(0);
 
+  useEffect(()=>{
+    setWord(WORD_LIST[index])
+  },[index])
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if(word===userInput){
+      setResult("You Won!")
+    }
+    else{
+      setResult("You Lost!")
+    }
+  };
+  const handleInputChange = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const handleRestartClick = ()=>{
+    setResult("")
+    setIndex(index+1)
+    setUserInput("")
+    setFlashWord(true)
+  } 
 
   return (
-    <div class="mini-game-container">
-      <h2 class="mini-game-title">Mini Game</h2>
-      <p class="mini-game-word">{word}</p>
-      <form class="mini-game-form" onSubmit={handleFormSubmit}>
-        <input class="mini-game-input" type="text" value={userInput} onChange={handleInputChange} />
-        <button class="mini-game-button" type="submit">Check Answer</button>
-      </form>
+    <div className="mini-game-container">
+      <h2 className="mini-game-title">Mini Game</h2>
+      {setTimeout(() => {
+        return setFlashWord(false);
+      }, 500)}
+      {flashWord ? (
+        <p className="mini-game-word">{word}</p>
+      ) : (
+        result == "" &&
+        <form className="mini-game-form" onSubmit={handleFormSubmit}>
+          <input
+            class="mini-game-input"
+            type="text"
+            value={userInput}
+            onChange={handleInputChange}
+          />
+          <button className="mini-game-button" type="submit">
+            Check Answer
+          </button>
+        </form>
+      
+      )}
+
       {result && (
         <>
           <p class="mini-game-result">{result}</p>
-          <button class="mini-game-restart-button" onClick={handleRestartClick}>Restart</button>
+          <button class="mini-game-restart-button" onClick={handleRestartClick}>
+            Restart
+          </button>
         </>
       )}
     </div>
